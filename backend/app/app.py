2cbe8ef38043d8c88c133b4162689393a1118f05
ns-shashank <nsshashankl@gmail.com>
2025-08-06 19:03:47 +0530

    Initial commit - added backend and frontend

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 from datetime import datetime
 import re
 
 app = Flask(__name__)
+CORS(app)
 expenses = []
 
 def parse_expense(text):
